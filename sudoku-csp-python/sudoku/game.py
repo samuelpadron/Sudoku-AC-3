f52ea0dbad8de45dc2864ade78e1f4e9813ad9a4
Samuel Padron <spadron05@gmail.com>
2022-10-13 15:13:47 +0200

    Finished game template

--- sudoku-csp-python/sudoku/game.py
+++ sudoku-csp-python/sudoku/game.py
@@ -6,3 +6,21 @@ class Game:
         
     def show_sudoku(self) -> None:
         print(self.sudoku)
+
+    def solve(self) -> bool:
+        """Implementation of AC-3 algorithm
+
+        Returns:
+            bool: true if the constraints can be satisfied, else false
+        """
+        #TODO
+        return True
+
+    def valid_solution(self) -> bool:
+        """Checks the validity of a sudoku solution
+
+        Returns:
+            bool: true if the sudoku solution is correct
+        """
+        #TODO
+        return True
